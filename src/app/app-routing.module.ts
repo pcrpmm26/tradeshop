63344f2cb87dd7c6f74d0c0135586560b398dc63
pcrpmm26 <maysexrn@gmail.com>
2022-02-07 16:18:30 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { NewProductComponent } from './components/pages/new-product/new-product.component';
 import { ProductComponent } from './components/pages/product/product.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { RegisterComponent } from './components/pages/register/register.component';
+import { AccountUserComponent } from './components/pages/account-user/account-user.component';
+import { MenuComponent } from './components/pages/menu/menu.component';
+import { EditProductComponent } from './components/pages/edit-product/edit-product.component';
+import { ExchangeComponent } from './components/pages/exchange/exchange.component';
 
 
 const routes: Routes = [
@@ -18,6 +22,10 @@ const routes: Routes = [
     // { path: "product/edit/:id", component: EditProductComponent }
     {path : "login",component:LoginComponent},
     {path : "register",component:RegisterComponent},
+    {path : "accountuser",component:AccountUserComponent},
+    {path : "menu",component:MenuComponent},
+    {path : "editproduct",component:EditProductComponent},
+    {path : "exchange",component:ExchangeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './components/shared/navigation/navigation.component';
 
 
 import { NewProductComponent } from './components/pages/new-product/new-product.component';
@@ -14,19 +13,28 @@ import { ProductComponent } from './components/pages/product/product.component';
 
 import { LoginComponent } from './components/pages/login/login.component';
 import { RegisterComponent } from './components/pages/register/register.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { AccountUserComponent } from './components/pages/account-user/account-user.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { MenuComponent } from './components/pages/menu/menu.component';
+import { EditProductComponent } from './components/pages/edit-product/edit-product.component';
+import { ExchangeComponent } from './components/pages/exchange/exchange.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
     NewProductComponent,
     ProductComponent,
     LoginComponent,
     RegisterComponent,
     HomePageComponent,
+    AccountUserComponent,
+    NavigationComponent,
+    MenuComponent,
+    EditProductComponent,
+    ExchangeComponent
   ],
   imports: [
     BrowserModule,
